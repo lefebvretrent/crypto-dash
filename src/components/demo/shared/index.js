a359940f95400e4cd2d3968f23f361b594b1ee29
Trenton Lefebvre <000807941@mohawkcollege.ca>
2021-05-05 12:04:26 -0400

    test

--- src/components/demo/shared/index.js
+++
@@ -1,6 +0,0 @@
-export { default as DocHeader } from "./DocHeader";
-export { default as DocSectionHeader } from "./DocSectionHeader";
-export { default as DocSectionSubheader } from "./DocSectionSubheader";
-export { default as DocParagraph } from "./DocParagraph";
-export { default as DocCode } from "./DocCode";
-export { default as DocCodeHeader } from "./DocCodeHeader";
--- src/components/demo/utils/index.js
+++
@@ -1,5 +0,0 @@
-export const createImageCdnLink = (filename) => {
-  return `https://cdn.jsdelivr.net/gh/frontend-joe/assets@main/${filename}`;
-};
-
-export const isDev = process.env.NODE_ENV === "development";
